1de31a9c72081178b1bfd8122def9a6ccac21495
Dimitris Papakyriakopoulos <t8190137@aueb.gr>
2020-11-14 17:05:11 +0200

    GUI landing page

---
+++ GUI/Graphstat.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+
+/**
+ * @author dimitrispapakyriakopoylos
+ *
+ */
+import java.awt.*;
+
+import javax.swing.JLabel;
+
+
+
+public class Graphstat extends JLabel {
+
+	private int[] graphdata;
+	
+	public Graphstat() {
+		graphdata= new int[] {15, 40, 130, 250, 180, 300};
+		this.setBackground(Color.WHITE);
+		
+	}
+	
+	protected void paintComponent(Graphics graph) {
+		super.paintComponent(graph);
+		
+		Graphics2D mgraph = (Graphics2D) graph;
+		int i, height, width, posx, posy;
+		for (i=0;i<graphdata.length;i++) {
+			height = (this.getHeight()-30)/graphdata.length - graphdata.length ;
+			width = (int) graphdata[i];
+			posx = 0;
+			posy = height * i+5;
+			
+			mgraph.setColor(Color.ORANGE);
+			mgraph.fill(new Rectangle(posx, posy, width, height));
+		}
+	}
+	
+}
+
