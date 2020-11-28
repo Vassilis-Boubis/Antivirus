132676147d89408f1fde1e1af19d5bd5764a3fe7
Anastasis Bakaros <65020564+anastasisbak@users.noreply.github.com>
2020-11-29 01:13:00 +0200

    Deleted TestGeocoding.java and TestMap.java, created MapDisplay.java and TestGeoMap.java that combines the Geocoding and MapDisplay classes so it can display a map. Try it yourself to see thw results(just execute it).

---
+++ geocode/src/main/java/geocode/TestGeoMap.java
@@ -0,0 +1,79 @@
+package geocode;
+
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.jxmapviewer.JXMapViewer;
+
+public class TestGeoMap {
+
+	public static void main(String[] args) {
+
+        show();
+
+	}
+
+	//Creates GUI elements, does forward geocoding of desired location
+	public static void show() {
+
+		JPanel p = new JPanel();
+
+        JFrame frame = new JFrame();
+
+        frame = frameCreator(1400, 800);
+
+        frame.add(p);
+
+        String location = locationInput();
+
+        double[] ping = new double[2];
+
+        Geocoding geo = new Geocoding();
+
+        MapDisplay showMap = new MapDisplay();
+
+        JXMapViewer mapViewer = new JXMapViewer();
+
+        ping = geo.search(location);
+
+        mapViewer = showMap.create(ping[0], ping[1]);
+
+        p.getRootPane().add(mapViewer);
+
+        frame.setVisible(true);
+
+	}
+
+
+	//User inputs location
+	public static String locationInput() {
+
+		String location;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter address (Attica):");
+
+		location = input.nextLine();
+
+		input.close();
+
+		return location;
+	}
+
+	//Creates JFrame
+	public static JFrame frameCreator(int width, int length) {
+
+		JFrame frame = new JFrame("MAP");
+
+
+        frame.setSize(width, length);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        return frame;
+
+	}
+
+}
