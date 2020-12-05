3e327297397ec98c5dc5c95dd3fca8dfab45de3b
Vassilis-Boubis <73957615+Vassilis-Boubis@users.noreply.github.com>
2020-12-05 17:22:02 +0200

    deleted two unused variables

--- antivirusApp/src/main/java/geocode/Square.java
+++ antivirusApp/src/main/java/geocode/Square.java
@@ -3,14 +3,11 @@ package geocode;
 public class Square {
 
 	private static double[][] lat_long= new double[4][2];
-	private static double lat;
-	private static double lon;
 	
 	public static double[][] square(double[] table) {
-		lat=table[0]+0.009;
-		lon=table[1]-0.012;
-		lat_long[0][0]=lat;
-		lat_long[0][1]=lon;
+
+		lat_long[0][0]=table[0]+0.009;
+		lat_long[0][1]=table[1]-0.012;
 		lat_long[1][0]=lat_long[0][0];
 		lat_long[1][1]=lat_long[0][1]+0.024;
 		lat_long[2][0]=lat_long[1][0]-0.018;
