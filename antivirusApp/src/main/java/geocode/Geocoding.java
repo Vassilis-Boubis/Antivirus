1710b1110c90ba5d3efaeba096670b1b1eeb6070
Dimitris Papakyriakopoulos <t8190137@aueb.gr>
2020-12-27 17:13:32 +0200

    Combined geocode with square

--- antivirusApp/src/main/java/geocode/Geocoding.java
+++ antivirusApp/src/main/java/geocode/Geocoding.java
@@ -10,6 +10,14 @@ import com.byteowls.jopencage.model.JOpenCageResponse;
 public class Geocoding {
 
 	private static Geocoding gc;
+	private static double[][] coords= new double[4][2];
+
+	/*First variable of the table[] must be the latitude and the second must be the longitude
+	 * of the address.
+	 *
+	 * If you sum 0,009 and latitude, you will go 1km to north and if you sum 0,012 and longitude you will go 1km to east
+	 * With this logic we are able to find the bounds of the square that has sides of 1km and center the inserted coordinates.
+	 */
 
 	//Geocoding prevents the instantiation from any other class.
 	private Geocoding() { }
@@ -96,5 +104,23 @@ public class Geocoding {
 		}
 
 	}
+	
+	
+	public static double[][] getSquareCoords(double[] table) {
+
+		coords[0][0]=table[0]+0.009;
+		coords[0][1]=table[1]-0.012;
+		coords[1][0]=coords[0][0];
+		coords[1][1]=coords[0][1]+0.024;
+		coords[2][0]=coords[1][0]-0.018;
+		coords[2][1]=coords[1][1];
+		coords[3][0]=coords[2][0];
+		coords[3][1]=coords[2][1]-0.024;
+
+		return coords;
+	}
+	
+	
+	
 
 }
