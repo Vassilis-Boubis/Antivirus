dab9521cb7f0e8496172464be4b29ba99f8b8a40
Dimitris Papakyriakopoulos <t8190137@aueb.gr>
2021-01-05 12:27:12 +0200

    Added falsely deleted MapDisplay

---
+++ antivirusApp/src/main/java/geocode/MapDisplay.java
@@ -0,0 +1,41 @@
+package geocode;
+
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+
+
+
+public class MapDisplay {
+
+	//Returns a JXMapviewer object so it can be added to a JPanel
+	public JXMapViewer create(double lat, double lng) {
+
+	   	JXMapViewer mapViewer = new JXMapViewer();
+	   	mapViewer.setBounds(300, 200, 350, 250);
+
+        // Create a TileFactoryInfo for OpenStreetMap
+        TileFactoryInfo info = new OSMTileFactoryInfo();
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+        mapViewer.setTileFactory(tileFactory);
+
+
+        // Use 8 threads in parallel to load the tiles
+        tileFactory.setThreadPoolSize(8);
+
+        GeoPosition position = new GeoPosition(lat, lng);
+
+        // Zoom set to 3.
+        mapViewer.setZoom(1);
+
+        mapViewer.setAddressLocation(position);
+
+        return mapViewer;
+
+
+
+	}
+
+}
