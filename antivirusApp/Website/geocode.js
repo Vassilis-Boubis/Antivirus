c4ab03ce9280a8f3d6bb78abbb950ede02168e2d
Lefteris <t8190144@aueb.gr>
2021-01-05 13:23:09 +0200

    Format javascript code

--- antivirusApp/Website/geocode.js
+++ antivirusApp/Website/geocode.js
@@ -1,137 +1,168 @@
 
+document
+		.querySelector('.search-btn')
+		.addEventListener(
+				'click',
+				function() {
+					var inputValue = document
+							.querySelector('.search-bar-input').value;
+					var input = getUserInput();
+					console.log(input);
 
-document.querySelector('.search-btn').addEventListener('click',function(){
-  var inputValue = document.querySelector('.search-bar-input').value;
-   var input = getUserInput();
-  console.log(input);
+					var apikey = 'f3f9c6b869de4117ae7fd321aaea815d';
+					var latitude;
+					var longitude;
 
-   var apikey = 'f3f9c6b869de4117ae7fd321aaea815d';
-   var latitude;
-   var longitude;
+					var api_url = 'https://api.opencagedata.com/geocode/v1/json'
 
-   var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+					var request_url = api_url + '?' + '&q='
+							+ encodeURIComponent(input) + '&key=' + apikey
+							+ '&pretty=1' + '&no_annotations=1';
 
-   var request_url = api_url
-     + '?'
-     + '&q=' + encodeURIComponent(input)
-     + '&key=' + apikey
-     + '&pretty=1'
-     + '&no_annotations=1';
+					var request = new XMLHttpRequest();
+					request.open('GET', request_url, true);
 
-   var request = new XMLHttpRequest();
-   request.open('GET', request_url, true);
+					request.onload = function() {
 
-   request.onload = function() {
+						if (request.status == 200) {
+							// Success!
+							var data = JSON.parse(request.responseText);
+							latitude = data.results[0].geometry.lat;
+							longitude = data.results[0].geometry.lng;
+							console.log(latitude, longitude);
+							console.log(data.results);
 
-     if (request.status == 200){
-       // Success!
-       var data = JSON.parse(request.responseText);
-       latitude = data.results[0].geometry.lat;
-       longitude = data.results[0].geometry.lng;
-       console.log(latitude,longitude);
-       console.log(data.results);
+							if (typeof (data.results[0].components.municipality) !== 'undefined') {
+								alert("-On a 1km radius of "
+										+ input
+										+ " there have been recorded 12 new covid cases in the last 14 days.\n"
+										+ "-"
+										+ data.results[0].components.municipality
+										+ " has had 45 new covid cases in the last 14 days.");
 
-      if (typeof(data.results[0].components.municipality) !== 'undefined' ){
-       alert("-On a 1km radius of " + input + " there have been recorded 12 new covid cases in the last 14 days.\n" +
-       "-" + data.results[0].components.municipality +  " has had 45 new covid cases in the last 14 days.");
+							} else if (typeof (data.results[0].components.suburb) !== 'undefined') {
+								alert("-On a 1km radius of "
+										+ input
+										+ " there have been recorded 12 new covid cases in the last 14 days.\n"
+										+ "-"
+										+ data.results[0].components.suburb
+										+ " has had 45 new covid cases in the last 14 days.");
+							} else {
+								alert("-On a 1km radius of "
+										+ input
+										+ " there have been recorded 12 new covid cases in the last 14 days.\n"
+										+ "-"
+										+ data.results[0].components.town
+										+ " has had 45 new covid cases in the last 14 days.");
+							}
 
-     } else if (typeof(data.results[0].components.suburb) !== 'undefined') {
-        alert("-On a 1km radius of " + input + " there have been recorded 12 new covid cases in the last 14 days.\n" +
-        "-" + data.results[0].components.suburb +  " has had 45 new covid cases in the last 14 days.");
-       } else {
-        alert("-On a 1km radius of " + input + " there have been recorded 12 new covid cases in the last 14 days.\n" +
-        "-" + data.results[0].components.town +  " has had 45 new covid cases in the last 14 days.");     }
+							var map = L.map('map').setView(
+									[ latitude, longitude ], 15);
+							L
+									.tileLayer(
+											'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=ZAsJI4cOmQysZB4SLofQ',
+											{
+												tileSize : 512,
+												zoomOffset : -1,
+												minZoom : 1,
+												attribution : "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+												crossOrigin : true
 
-       var map = L.map('map').setView([latitude, longitude], 15);
-       L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=ZAsJI4cOmQysZB4SLofQ',{
-         tileSize: 512,
-         zoomOffset: -1,
-         minZoom: 1,
-         attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-         crossOrigin: true
+											}).addTo(map);
+							var marker = L.marker([ latitude, longitude ])
+									.addTo(map);
 
-       }).addTo(map);
-         var marker = L.marker([latitude,longitude]).addTo(map);
+						} else if (request.status <= 500) {
+							// We reached our target server, but it returned an
+							// error
 
-     } else if (request.status <= 500){
-       // We reached our target server, but it returned an error
+							console.log("unable to geocode! Response code: "
+									+ request.status);
+							var data = JSON.parse(request.responseText);
+							console.log(data.status.message);
+						} else {
+							console.log("server error");
+						}
+					};
 
-       console.log("unable to geocode! Response code: " + request.status);
-       var data = JSON.parse(request.responseText);
-       console.log(data.status.message);
-     } else {
-       console.log("server error");
-     }
-   };
+					request.onerror = function() {
+						// There was a connection error of some sort
+						console.log("unable to connect to server");
+					};
 
-   request.onerror = function() {
-     // There was a connection error of some sort
-     console.log("unable to connect to server");
-   };
+					request.send(); // make the request
+				});
 
-   request.send();  // make the request
-});
+document
+		.querySelector(".search-bar")
+		.addEventListener(
+				'keyup',
+				function(e) {
+					if (e.which === 13) {
 
+						var input = getUserInput();
+						console.log(input);
 
-document.querySelector(".search-bar").addEventListener('keyup',function(e){if(e.which === 13) {
+						var apikey = 'f3f9c6b869de4117ae7fd321aaea815d';
+						var latitude;
+						var longitude;
 
-  var input = getUserInput();
-  console.log(input);
+						var api_url = 'https://api.opencagedata.com/geocode/v1/json'
 
-  var apikey = 'f3f9c6b869de4117ae7fd321aaea815d';
-  var latitude;
-  var longitude;
+						var request_url = api_url + '?' + '&q='
+								+ encodeURIComponent(input) + '&key=' + apikey
+								+ '&pretty=1' + '&no_annotations=1';
 
-  var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+						var request = new XMLHttpRequest();
+						request.open('GET', request_url, true);
 
-  var request_url = api_url
-    + '?'
-    + '&q=' + encodeURIComponent(input)
-    + '&key=' + apikey
-    + '&pretty=1'
-    + '&no_annotations=1';
+						request.onload = function() {
 
-  var request = new XMLHttpRequest();
-  request.open('GET', request_url, true);
+							if (request.status == 200) {
+								// Success!
+								var data = JSON.parse(request.responseText);
+								latitude = data.results[0].geometry.lat;
+								longitude = data.results[0].geometry.lng;
+								console.log(latitude, longitude);
 
-  request.onload = function() {
+								var map = L.map('map').setView(
+										[ latitude, longitude ], 15);
+								L
+										.tileLayer(
+												'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=ZAsJI4cOmQysZB4SLofQ',
+												{
+													tileSize : 512,
+													zoomOffset : -1,
+													minZoom : 1,
+													attribution : "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+													crossOrigin : true
 
-    if (request.status == 200){
-      // Success!
-      var data = JSON.parse(request.responseText);
-      latitude = data.results[0].geometry.lat;
-      longitude = data.results[0].geometry.lng;
-      console.log(latitude,longitude);
+												}).addTo(map);
+								var marker = L.marker([ latitude, longitude ])
+										.addTo(map);
 
-      var map = L.map('map').setView([latitude, longitude], 15);
-      L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=ZAsJI4cOmQysZB4SLofQ',{
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        crossOrigin: true
+							} else if (request.status <= 500) {
+								// We reached our target server, but it returned
+								// an error
 
-      }).addTo(map);
-        var marker = L.marker([latitude,longitude]).addTo(map);
+								console
+										.log("unable to geocode! Response code: "
+												+ request.status);
+								var data = JSON.parse(request.responseText);
+								console.log(data.status.message);
+							} else {
+								console.log("server error");
+							}
+						};
 
-    } else if (request.status <= 500){
-      // We reached our target server, but it returned an error
+						request.onerror = function() {
+							// There was a connection error of some sort
+							console.log("unable to connect to server");
+						};
 
-      console.log("unable to geocode! Response code: " + request.status);
-      var data = JSON.parse(request.responseText);
-      console.log(data.status.message);
-    } else {
-      console.log("server error");
-    }
-  };
-
-  request.onerror = function() {
-    // There was a connection error of some sort
-    console.log("unable to connect to server");
-  };
-
-  request.send();  // make the request
-}});
+						request.send(); // make the request
+					}
+				});
 
 function getUserInput() {
 	var inputValue = document.querySelector('.search-bar-input').value;
